659aa6eb9c97338698b69f175543957008df6662
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 16:18:04 +0800

    style: add test and benchmark copyright headers

--- benches/thread_pool_bench.rs
+++ benches/thread_pool_bench.rs
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 /*!
  * Benchmark for [`qubit_thread_pool::task::service::ThreadPool`].
  */
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,1 +1,10 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 mod thread_pool;
