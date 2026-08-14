71182ea302c4b7b79355209aae585b0270a58b7a
Haixing Hu <starfish.hu@gmail.com>
2026-08-15 02:55:22 +0800

    style: use explicit test imports

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -8,10 +8,13 @@
 mod dynamic;
 mod fixed;
 
+use qubit_thread_pool::FixedThreadPool;
+use qubit_thread_pool::ThreadPool;
+
 #[test]
 fn test_lib_exports_primary_executor_types() {
-    let dynamic = qubit_thread_pool::ThreadPool::new(1);
-    let fixed = qubit_thread_pool::FixedThreadPool::new(1);
+    let dynamic = ThreadPool::new(1);
+    let fixed = FixedThreadPool::new(1);
 
     assert!(dynamic.is_ok());
     assert!(fixed.is_ok());
